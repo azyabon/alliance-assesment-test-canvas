387f2348755617dd50bc8e567f66465dc0ded283
Andrey Azyabin <73127870+azyabon@users.noreply.github.com>
2025-07-17 16:40:21 +0700

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: './',
+  base: '/alliance-assesment-test-canvas/',
   plugins: [react()],
   optimizeDeps: {
     include: ["konva"],
